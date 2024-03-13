c1c2f52f7fc69a7c02d9d942c9022592b137930f
Mahmoud Osama <mosss2468@gmail.com>
2024-03-14 00:08:57 +0200

    crate a file to solve assign

---
+++ python/assign1to10.py
@@ -0,0 +1,22 @@
+#assign 1
+
+#this is comment
+#in this file we solving the assignment
+
+#assign 2
+name = "mahmoud"
+age = "21"
+country = "Egypt"
+
+# assign 3
+print("Name : "+name)
+print("Age : "+age)
+print("Country : "+country)
+
+# assign 4
+print(f"Hello, My Name Is {name} And Iam {age}Years Old and I Live in {country}.")
+
+# assign 5
+print(type(name))
+print(type(age))
+print(type(country))
